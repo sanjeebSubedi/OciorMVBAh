b90c47e130264f5ce073d733878ede33c5f0b4e5
Sanjeeb Subedi <sanjeebsubedi4@gmail.com>
2025-06-26 20:59:29 -0500

    add simple predicate function

---
+++ utilities/predicate.py
@@ -0,0 +1,11 @@
+def predicate(w: str) -> bool:
+    return w.startswith("value_from_")
+
+
+def main():
+    print(predicate("value_from_node_1"))
+    print(predicate("node_2"))
+
+
+if __name__ == "__main__":
+    main()
